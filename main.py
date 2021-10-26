6515dd048817670722ac3c708e227862ccb94c92
Kohei Watabe <k_watabe@vos.nagaokaut.ac.jp>
2021-10-26 16:03:25 +0900

    コマンドライン引数の指定方法を修正

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
-from config import PARAMS
-from utils import dump_params
+import argparse
+from config import common_args, Parameters
+from utils import dump_params, setup_params
 
 
 def main():
-    result_dir = f'result/{PARAMS.run_date}'  # 結果出力ディレクトリ
+    parser = argparse.ArgumentParser()
+    parser = common_args(parser)  # コマンドライン引数引数を読み込み
+    # parser.add_argument("--main")  # 実行スクリプト固有のコマンドライン引数があればここに記入する．
+    args = parser.parse_args()
+    params = Parameters(**setup_params(vars(args), args.parameters))  # args，run_date，git_revisionなどを追加した辞書を取得
+
+    result_dir = f'result/{params.run_date}'  # 結果出力ディレクトリ
     os.mkdir(result_dir)  # 実行日時を名前とするディレクトリを作成
-    dump_params(PARAMS, f'{result_dir}')  # パラメータを出力
+    dump_params(params, f'{result_dir}')  # パラメータを出力
     
     # do something...
+    
+    print(params)
 
 
 if __name__ == "__main__":
